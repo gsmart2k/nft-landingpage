d0cb243b20d52cd5ed1f5d729d65981b5eecca17
GSMART <ogunrinadegbolahan2000@gmail.com>
2024-12-13 20:07:40 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,281 @@
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Link from "next/link";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<div className="flex flex-col h-screen w-full bg-[url('/heroimage.png')] bg-no-repeat bg-cover ">
+			<div className="flex flex-row items-center justify-between w-full p-10">
+				<div className="flex flex-row items-center space-x-10">
+					<Image src="/logo.png" width={50} height={50} />
+					<nav className="flex flex-row space-x-10 text-sm">
+						<Link href="">Home</Link>
+						<Link href="">MarketPlace</Link>
+						<Link href="">About</Link>
+						<Link href="">Wallet</Link>
+					</nav>
+				</div>
+				<div className="flex flex-row justify-between space-x-10">
+					<div className="flex items-center bg-white/20 backdrop-blur-lg rounded-lg shadow-lg px-4 py-2">
+						<MagnifyingGlassIcon
+							width={20}
+							height={20}
+							className="text-white"
+						/>
+						<input
+							className="bg-transparent text-white placeholder-white/70 focus:outline-none ml-2 text-sm"
+							placeholder="Search By Creator"
+						/>
+					</div>
+					<button className="rounded-full bg-none bg-transparent  text-sm px-7 py-3 shadow-custom shadow-cyan-500/50">
+						Join Waitlist
+					</button>
+				</div>
+			</div>
+			{/* HERO SECTION BELOW */}
+			<div className="flex flex-col mt-10 px-10">
+				<h2
+					style={{ WebkitTextStroke: "2px #26135080", color: "transparent" }}
+					className="font-custom text-[60px] bg-gradient-to-r from-white to-[#5FECFC] text-transparent bg-clip-text w-9/12">
+					Discover and Trade Extraordinary Digital Arts
+				</h2>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+				<h5 className="text-xl text-gray-400 font-thin">
+					Join the vibrant community of creators and collectors
+				</h5>
+				<button className="bg-gradient-to-r from-[#2575FF] via-[#5FECFC] to-[#57E493] rounded-full text-black w-fit mt-10 shadow-custom shadow-cyan-500/50 text-lg px-10 py-3">
+					Explore Now
+				</button>
+				<div className="flex flex-row rounded-3xl w-fit mt-10 p-5 space-x-14 bg-[#30303080]">
+					<div className="flex flex-col space-y-1 justify-center items-center">
+						<h3 className="font-custom text-3xl">28k+</h3>
+						<h6 className="text-[#5FECFC]">Satisfied Users</h6>
+					</div>
+					<div className="flex flex-col space-y-2 justify-center items-center">
+						<h3 className="font-custom text-3xl">18k+</h3>
+						<h6 className="text-[#5FECFC]">Artists</h6>
+					</div>
+					<div className="flex flex-col space-y-2 justify-center items-center">
+						<h3 className="font-custom text-3xl">20k+</h3>
+						<h6 className="text-[#5FECFC]">Distributors</h6>
+					</div>
+				</div>
+			</div>
+
+			{/* COMPANY LOGOS  */}
+			<div className="flex flex-row w-full p-10 justify-between mt-5">
+				<Image width={150} height={150} src="/logos/coinbase.svg" />
+				<Image width={150} height={150} src="/logos/spotify.svg" />
+				<Image width={150} height={150} src="/logos/slack.svg" />
+				<Image width={150} height={150} src="/logos/dropbox.svg" />
+				<Image width={150} height={150} src="/logos/webflow.svg" />
+			</div>
+
+			{/* ABOUT NFTS */}
+
+			<div className="flex flex-row w-full mt-10 justify-center items-center px-20 py-10 space-x-10">
+				<div className="flex w-6/12">
+					<Image src="/aboutnft.png" width={500} height={500} />
+				</div>
+				<div className="w-6/12 space-y-5">
+					<h2 className="font-custom bg-gradient-to-r from-white via-[#5FECFC] to-[#5FECFC] bg-clip-text text-transparent text-4xl">
+						About NFTS
+					</h2>
+					<h5 className="text-xl font-extralight leading-loose">
+						Non-Fungible Tokens (NFTs) are unique digital assets verified with
+						blockchain technology. They represent ownership of specific items
+						like artwork, music, and more, offering artists and creators a new
+						way to monetize their work.
+					</h5>
+				</div>
+			</div>
+
+			{/* FEATURED NFTS */}
+			<div className="flex flex-col w-full justify-center items-center py-10 px-20">
+				<h2 className="font-custom bg-gradient-to-r p-5 from-white via-[#5FECFC] to-[#5FECFC] bg-clip-text text-transparent text-4xl">
+					Featured NFTS
+				</h2>
+				<div className="grid grid-rows-2 grid-flow-col gap-x-10 gap-y-5 mt-10">
+					<div className="flex flex-col bg-[#30303099] rounded-md p-3 w-fit">
+						<Image
+							src="/featured/nft1.png"
+							width={500}
+							height={500}
+							alt="NFT"
+						/>
+						<div className="flex flex-row items-center justify-between p-5">
+							<div className="flex flex-col">
+								<h4 className="font-custom text-lg">Eternal</h4>
+								<h4 className="text-[#815EE3]">1.2Eth</h4>
+							</div>
+							<h3>1 of 12</h3>
+						</div>
+						<hr className="flex m-auto w-11/12" />
+						<button className="text-[#5FECFC] p-5">Place a bid</button>
+					</div>
+					<div className="flex flex-col bg-[#30303099] rounded-md p-3 w-fit">
+						<Image
+							src="/featured/nft2.png"
+							width={500}
+							height={500}
+							alt="NFT"
+						/>
+						<div className="flex flex-row items-center justify-between p-5">
+							<div className="flex flex-col">
+								<h4 className="font-custom text-lg">Eternal</h4>
+								<h4 className="text-[#815EE3]">1.2Eth</h4>
+							</div>
+							<h3>1 of 12</h3>
+						</div>
+						<hr className="flex m-auto w-11/12" />
+						<button className="text-[#5FECFC] p-5">Place a bid</button>
+					</div>
+					<div className="flex flex-col bg-[#30303099] rounded-md p-3 w-fit">
+						<Image
+							src="/featured/nft3.png"
+							width={500}
+							height={500}
+							alt="NFT"
+						/>
+						<div className="flex flex-row items-center justify-between p-5">
+							<div className="flex flex-col">
+								<h4 className="font-custom text-lg">Eternal</h4>
+								<h4 className="text-[#815EE3]">1.2Eth</h4>
+							</div>
+							<h3>1 of 12</h3>
+						</div>
+						<hr className="flex m-auto w-11/12" />
+						<button className="text-[#5FECFC] p-5">Place a bid</button>
+					</div>
+					<div className="flex flex-col bg-[#30303099] rounded-md p-3 w-fit">
+						<Image
+							src="/featured/nft4.png"
+							width={500}
+							height={500}
+							alt="NFT"
+						/>
+						<div className="flex flex-row items-center justify-between p-5">
+							<div className="flex flex-col">
+								<h4 className="font-custom text-lg">Eternal</h4>
+								<h4 className="text-[#815EE3]">1.2Eth</h4>
+							</div>
+							<h3>1 of 12</h3>
+						</div>
+						<hr className="flex m-auto w-11/12" />
+						<button className="text-[#5FECFC] p-5">Place a bid</button>
+					</div>
+					<div className="flex flex-col bg-[#30303099] rounded-md p-3 w-fit">
+						<Image
+							src="/featured/nft5.png"
+							width={500}
+							height={500}
+							alt="NFT"
+						/>
+						<div className="flex flex-row items-center justify-between p-5">
+							<div className="flex flex-col">
+								<h4 className="font-custom text-lg">Eternal</h4>
+								<h4 className="text-[#815EE3]">1.2Eth</h4>
+							</div>
+							<h3>1 of 12</h3>
+						</div>
+						<hr className="flex m-auto w-11/12" />
+						<button className="text-[#5FECFC] p-5">Place a bid</button>
+					</div>
+					<div className="flex flex-col bg-[#30303099] rounded-md p-3 w-fit">
+						<Image
+							src="/featured/nft1.png"
+							width={500}
+							height={500}
+							alt="NFT"
+						/>
+						<div className="flex flex-row items-center justify-between p-5">
+							<div className="flex flex-col">
+								<h4 className="font-custom text-lg">Eternal</h4>
+								<h4 className="text-[#815EE3]">1.2Eth</h4>
+							</div>
+							<h3>1 of 12</h3>
+						</div>
+						<hr className="flex m-auto w-11/12" />
+						<button className="text-[#5FECFC] p-5">Place a bid</button>
+					</div>
+				</div>
+			</div>
+
+			{/* HOW IT WORKS */}
+			<div className="flex flex-col m-0 py-32 px-10 bg-gradient-to-br from-[#130B3B] via-[#130B3B] to-pink-950 w-full space-y-10">
+				<h2 className="font-custom bg-gradient-to-r from-white to-[#5FECFC] bg-clip-text text-transparent text-4xl text-center">
+					How It Works
+				</h2>
+				<div className="flex flex-row items-center justify-center space-x-10">
+					<div className="flex flex-col w-4/12 bg-[#30303099] p-10 items-center justify-center space-y-5 rounded-lg">
+						<Image
+							src="/icons/wallet.png"
+							width={40}
+							height={40}
+							alt="wallet"
+						/>
+						<h3 className="font-custom text-lg">Connect Your Wallet</h3>
+						<p className="text-center text-sm text-gray-400 text-ellipsis">
+							Securely link your digital wallet to start buying and selling NFTs
+							on our platform.
+						</p>
+					</div>
+					<div className="flex flex-col w-4/12 bg-[#30303099] p-10 items-center justify-center space-y-5 rounded-lg">
+						<Image
+							src="/icons/discovery.png"
+							width={40}
+							height={40}
+							alt="wallet"
+						/>
+						<h3 className="font-custom text-lg">Discover Art</h3>
+						<p className="text-center text-sm text-gray-400 text-ellipsis">
+							Explore a diverse collection of digital artworks from emerging and
+							established artists.
+						</p>
+					</div>
+					<div className="flex flex-col w-4/12 bg-[#30303099] p-10 items-center justify-center space-y-5 rounded-lg">
+						<Image src="/icons/card.png" width={40} height={40} alt="wallet" />
+						<h3 className="font-custom text-lg">Trade NFTs</h3>
+						<p className="text-center text-sm text-gray-400 text-ellipsis">
+							Buy or sell NFTs through simple transactions, with all details
+							transparently handled on the blockchain.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			{/* STAY IN THE LOOP */}
+			<div className="flex justify-center items-center p-10">
+				<div className="flex p-[1px] bg-black text-white rounded-lg border-1 border-transparent bg-clip-border bg-gradient-to-r from-[#2575FF] via-[#5FECFC] to-[#57E493] w-full">
+					<div className="flex flex-col items-center justify-center bg-gray-800 w-full rounded-md py-20 px-10 space-y-10">
+						<div className="flex flex-col space-y-3 items-center justify-center">
+							<h3 className="font-custom text-2xl">Stay In The Loop</h3>
+							<p className="text-sm text-gray-500">
+								Get the latest NFT updates and exclusive offers straight to your
+								inbox!
+							</p>
+						</div>
+						<div className="flex space-x-5 items-center justify-center w-8/12">
+							<input
+								className="bg-transparent p-3  bg-gray-500 rounded-xl w-5/12"
+								placeholder="Enter your email address"
+							/>
+							<button className="rounded-full bg-none bg-black  text-md px-7 py-3 shadow-custom shadow-cyan-500/50">
+								View More
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* FOOTER */}
+
+			<hr className="opacity-30" />
+
+			<div className="flex p-10 justify-center items-center">
+				<h4 className="text-sm text-center ">
+					&copy; Designed By Crownz | Developed By OMOGBOLAHAN
+				</h4>
+			</div>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			boxShadow: {
+				custom: "2px 2px 20px 3px rgba(0, 0, 0, 0.3)",
+			},
+			fontFamily: {
+				custom: ["Conthrax-SemiBold", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
